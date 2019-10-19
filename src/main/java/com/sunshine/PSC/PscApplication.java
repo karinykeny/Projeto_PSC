f9d2e6080ad2e10761ad56d6720eea2cc0837fe3
Kariny Keny <karinykeny@gmail.com>
2019-10-18 23:01:58 -0300

    Base do projeto

---
+++ src/main/java/com/sunshine/PSC/PscApplication.java
@@ -0,0 +1,13 @@
+package com.sunshine.PSC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PscApplication.class, args);
+	}
+
+}
