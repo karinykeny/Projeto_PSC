6a026ad89b36e51dd909c8c78617aa75a92c4a3c
valterapolinario <valter_123@hotmail.com>
2019-10-19 08:45:41 -0300

    classes dominio implementada

---
+++ src/main/java/com/sunshine/PSC/dominio/Funcionario.java
@@ -0,0 +1,115 @@
+package com.sunshine.PSC.dominio;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Funcionario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String senha;
+	private String telefone;
+	@OneToMany(mappedBy = "funcionario")
+	private List<Cliente> clientes = new ArrayList<>();
+	@OneToMany(mappedBy = "funcionario")
+	private List<Reserva> reservas = new ArrayList<>();
+
+	public Funcionario() {
+
+	}
+
+	public Funcionario(Integer id, String nome, String cpf, String email, String senha, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		this.telefone = telefone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/sunshine/PSC/dominio/Reserva.java
@@ -0,0 +1,145 @@
+package com.sunshine.PSC.dominio;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Reserva implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer nPessoas;
+	private Date dataEntrada;
+	private Date dataSaida;
+	private Double precoDiaria;
+	private Double total;
+
+	@ManyToMany
+	@JoinTable(name = "RESERVA_QUARTOS", joinColumns = @JoinColumn(name = "reserva_id"), inverseJoinColumns = @JoinColumn(name = "quarto_id"))
+	private List<Quarto> quartos = new ArrayList<>();
+	@JsonIgnore
+	@ManyToOne
+	private Cliente cliente;
+	@JsonIgnore
+	@ManyToOne
+	private Funcionario funcionario;
+
+	public Reserva() {
+
+	}
+
+	public Reserva(Integer id, Integer nPessoas, Date dataEntrada, Date dataSaida, Double precoDiaria, Double total) {
+		super();
+		this.id = id;
+		this.nPessoas = nPessoas;
+		this.dataEntrada = dataEntrada;
+		this.dataSaida = dataSaida;
+		this.setPrecoDiaria(precoDiaria);
+		this.setTotal(total);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getnPessoas() {
+		return nPessoas;
+	}
+
+	public void setnPessoas(Integer nPessoas) {
+		this.nPessoas = nPessoas;
+	}
+
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(Date dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(Date dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+
+	public List<Quarto> getQuartos() {
+		return quartos;
+	}
+
+	public void setQuartos(List<Quarto> quartos) {
+		this.quartos = quartos;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Double getPrecoDiaria() {
+		return precoDiaria;
+	}
+
+	public void setPrecoDiaria(Double precoDiaria) {
+		this.precoDiaria = precoDiaria;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reserva other = (Reserva) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
