ddcdf02fe9ffbf950672d58aae1aa6d1aff5958e
heitorAlbu <heitor.albuquerque@gmail.com>
2019-10-20 07:28:29 -0700

    inserção de registros de quartos no banco h2 e implementação do método listar para Quartos

--- src/main/java/com/sunshine/PSC/controllers/QuartoController.java
+++ src/main/java/com/sunshine/PSC/controllers/QuartoController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,8 +29,15 @@ public class QuartoController {
 	@RequestMapping(value="/Quarto/create", method=RequestMethod.POST)
 	public String create(Quarto quarto) {
 	
-		dao.create(quarto);
+		dao.save(quarto);
 		return "quarto/confirmacao";
 	}
+	@RequestMapping(value="/Quarto/listar", method=RequestMethod.GET)
+	public String listar(ModelMap model) {
+				
+		model.addAttribute("quartos", dao.findAll());
+		return "quarto/index";	
+	}
+	
 	
 }
--- src/main/java/com/sunshine/PSC/dao/QuartoDao.java
+++ src/main/java/com/sunshine/PSC/dao/QuartoDao.java
@@ -3,23 +3,14 @@ package com.sunshine.PSC.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sunshine.PSC.dominio.Quarto;
 
 @Repository
-@Transactional
-public class QuartoDao {
+//@Transactional
+public interface QuartoDao extends JpaRepository<Quarto, Integer> {
 
-	@PersistenceContext
-	private EntityManager manager;
-	
-	
-
-	public void create(Quarto quarto) {
-		
-		manager.persist(quarto);
-	}
 }
