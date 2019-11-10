fdf917b6ea0f821d2830467235f57f2e67b2070d
heitorAlbu <heitor.albuquerque@gmail.com>
2019-11-10 14:18:39 -0800

    realizar verificação pom.xml dependencia session (não está funcionando)

--- src/main/java/com/sunshine/PSC/security/WebSecurityConfig.java
+++ src/main/java/com/sunshine/PSC/security/WebSecurityConfig.java
@@ -28,10 +28,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
 	protected void configure(HttpSecurity http) throws Exception{ //método de configurações
 		http.csrf().disable().authorizeRequests()//crsf provê vários tipos de segurança para a aplicação
 		.antMatchers(HttpMethod.GET, "/").permitAll()//todos os me
-		.antMatchers(HttpMethod.GET, "/quarto/listarQuartos").hasRole("ADMIN")
+		/*.antMatchers(HttpMethod.GET, "/quarto/listarQuartos").hasRole("ADMIN")
 		.antMatchers(HttpMethod.GET, "quarto/cadastrarQuartos").hasRole("USER")
+		.antMatchers(HttpMethod.GET, "/reserva/listarReservas").hasRole("ADMIN")
 		
-		.anyRequest().authenticated() //para todas as demais requisições precisará de Autenticação
+		.anyRequest().authenticated() //para todas as demais requisições precisará de Autenticação*/
 		.and().formLogin().permitAll()// para que a pessoa acesse o formulário de login do spring security e assim ter acesso... 
 		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 		http.headers().frameOptions().disable();
