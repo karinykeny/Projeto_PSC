e83b39d0dd4a7f70646d0443151322e989cc57d6
PedroSantosUnibratec <47306345+PedroSantosUnibratec@users.noreply.github.com>
2019-11-11 17:39:00 -0300

    Import AbstractHttpSessionApplicationInitializer
    Fazendo a importação da classe AbstractHttpSessionApplicationInitializer

--- src/main/java/com/sunshine/PSC/session/SessionConfig.java
+++ src/main/java/com/sunshine/PSC/session/SessionConfig.java
@@ -1,8 +1,9 @@
 package com.sunshine.PSC.session;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
-public class SessionConfig extends AbstractHttpSessionApplicationInitializer   {
+public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
 
 }
