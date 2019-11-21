6e7b1151d67e05e492c71f5c3007431395b40f4c
heitorAlbu <heitor.albuquerque@gmail.com>
2019-11-20 21:56:18 -0300

    commit

--- src/main/java/com/sunshine/PSC/session/SessionConfig.java
+++ src/main/java/com/sunshine/PSC/session/SessionConfig.java
@@ -3,6 +3,6 @@ package com.sunshine.PSC.session;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SessionConfig extends AbstractHttpSessionApplicationInitializer   {
+public class SessionConfig {
 
 }
