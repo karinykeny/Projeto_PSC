afa7af2235c9d489d2007cc3ecb66ebbadabef89
valterapolinario <valter_123@hotmail.com>
2019-11-23 14:42:11 -0300

    implementando pagina pagamento

--- src/main/java/com/sunshine/PSC/security/WebSecurityConfig.java
+++ src/main/java/com/sunshine/PSC/security/WebSecurityConfig.java
@@ -18,43 +18,50 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
-	//"WebSecurityConfigurerAdapter" - Classe nativa do Spring Security 
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	// "WebSecurityConfigurerAdapter" - Classe nativa do Spring Security
 
 	@Autowired
 	private ImplementsUserDetailService userDetailsService;
-	
-	
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{ //método de configurações
-		http.csrf().disable().authorizeRequests()//crsf provê vários tipos de segurança para a aplicação
-		/*.antMatchers(HttpMethod.GET, "/").permitAll()//todos os me
+	protected void configure(HttpSecurity http) throws Exception { // método de configurações
+		http.csrf().disable().authorizeRequests()// crsf provê vários tipos de segurança para a aplicação
+				/*
+				 * .antMatchers(HttpMethod.GET, "/").permitAll()//todos os me
+				 * 
+				 * 
+				 * .antMatchers(HttpMethod.GET, "quarto/cadastrarQuartos").hasRole("USER")
+				 * .antMatchers(HttpMethod.GET, "/reserva/listarReservas").hasRole("ADMIN")
+				 * 
+				 * //.anyRequest().authenticated() //para todas as demais requisições precisará
+				 * de Autenticação
+				 */
 
+				// .anyRequest().authenticated() //para todas as demais requisições precisará de
+				// Autenticação*/
 
-		.antMatchers(HttpMethod.GET, "quarto/cadastrarQuartos").hasRole("USER")
-		.antMatchers(HttpMethod.GET, "/reserva/listarReservas").hasRole("ADMIN")
+				// .anyRequest().authenticated() //para todas as demais requisições precisará de
+				// Autenticação
 
-		//.anyRequest().authenticated() //para todas as demais requisições precisará de Autenticação*/
-		
-		.anyRequest().authenticated() //para todas as demais requisições precisará de Autenticação*/
-		
-		//.anyRequest().authenticated() //para todas as demais requisições precisará de Autenticação
-
-		.and().formLogin().permitAll()// para que a pessoa acesse o formulário de login do spring security e assim ter acesso... 
-		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+				.and().formLogin().permitAll()// para que a pessoa acesse o formulário de login do spring security e
+												// assim ter acesso...
+				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 		http.headers().frameOptions().disable();
 	}
-	
+
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{ // método que define como será feita a autenticação em memória através da classe "AuthenticationManagerBuilder" 
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception { // método que define como será feita
+																					// a autenticação em memória através
+																					// da classe
+																					// "AuthenticationManagerBuilder"
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
-/*
-	@Override
-	public void configure(WebSecurity web) throws Exception{
-		web.ignoring().antMatchers("/css/**");
-	}*/
-	//Esse método serve para que o spring security não bloqueie as páginas estáticas quando o browser pedir o acesso a estas pastas
-	
-	
+	/*
+	 * @Override public void configure(WebSecurity web) throws Exception{
+	 * web.ignoring().antMatchers("/css/**"); }
+	 */
+	// Esse método serve para que o spring security não bloqueie as páginas
+	// estáticas quando o browser pedir o acesso a estas pastas
+
 }
